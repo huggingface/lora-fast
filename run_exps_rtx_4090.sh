fb24321e72e4e45b7f7617bb28bf75e2146750aa
Benjamin Bossan <benjamin.bossan@gmail.com>
2025-07-23 12:15:42 +0200

    Some clean ups
    - Formatting
    - Type annotations
    - By default, save all results in a results/ directory
    
    I would also suggest to remove the last sentence from the 2nd prompt, as
    I get the following warning:
    
    > The following part of your input was truncated because CLIP can only
    handle sequences up to 77 tokens: ['in the lower corner show a price of
    1 5 cents and the date sep 2 0 2 4']

--- run_exps_rtx_4090.sh
+++ run_exps_rtx_4090.sh
@@ -1,47 +1,46 @@
-
-
-python run_benchmark.py --disable_fa3 --quantize_t5 --out_dir=no_fa3  2>& 1 | tee no_fa3.txt
+python run_benchmark.py --disable_fa3 --quantize_t5 --out_dir=results/no_fa3  2>& 1 | tee results/no_fa3.txt
 
 python run_benchmark.py --disable_fa3 --quantize_t5 \
-    --disable_compile --out_dir=no_compile_fa3  2>& 1 | tee no_compile_fa3.txt
+    --disable_compile --out_dir=results/no_compile_fa3  2>& 1 | tee results/no_compile_fa3.txt
+
+# Experiments below with fp8 disabled will result in OOM errors:
 
 python run_benchmark.py --disable_fa3 --disable_fp8 --offload \
-    --disable_recompile_error --out_dir=no_fa3_fp8_nf4 2>& 1 | tee no_fa3_fp8_nf4.txt
+    --disable_recompile_error --out_dir=results/no_fa3_fp8_nf4 2>& 1 | tee results/no_fa3_fp8_nf4.txt
 
 python run_benchmark.py --disable_fa3 --disable_fp8 --quantize_t5 --offload \
-    --disable_recompile_error --out_dir=no_fa3_fp8 2>& 1 | tee no_fa3_fp8.txt
+    --disable_recompile_error --out_dir=results/no_fa3_fp8 2>& 1 | tee results/no_fa3_fp8.txt
 
 python run_benchmark.py --disable_fa3 --disable_fp8 --offload \
     --disable_recompile_error --disable_compile \
-    --out_dir=no_fa3_fp8_nf4_compile 2>& 1 | tee no_fa3_fp8_nf4_compile.txt
+    --out_dir=results/no_fa3_fp8_nf4_compile 2>& 1 | tee results/no_fa3_fp8_nf4_compile.txt
 
 python run_benchmark.py --disable_fa3 --disable_fp8 --quantize_t5 --offload \
     --disable_recompile_error --disable_compile \
-    --out_dir=no_fa3_fp8_compile 2>& 1 | tee no_fa3_fp8_compile.txt
+    --out_dir=results/no_fa3_fp8_compile 2>& 1 | tee results/no_fa3_fp8_compile.txt
 
 python run_benchmark.py \
     --disable_fa3 --disable_fp8 --offload \
     --disable_hotswap \
     --disable_recompile_error \
-    --out_dir=no_fa3_fp8_nf4_hotswap 2>& 1 | tee no_fa3_fp8_nf4_hotswap.txt
+    --out_dir=results/no_fa3_fp8_nf4_hotswap 2>& 1 | tee results/no_fa3_fp8_nf4_hotswap.txt
 
 python run_benchmark.py \
     --disable_fa3 --disable_fp8 --offload --quantize_t5 \
     --disable_hotswap \
     --disable_recompile_error \
-    --out_dir=no_fa3_fp8_hotswap 2>& 1 | tee no_fa3_fp8_hotswap.txt
+    --out_dir=results/no_fa3_fp8_hotswap 2>& 1 | tee results/no_fa3_fp8_hotswap.txt
 
 python run_benchmark.py \
     --disable_fa3 --disable_fp8 --offload \
     --disable_hotswap \
     --disable_compile \
     --disable_recompile_error \
-    --out_dir=no_fa3_fp8_nf4_hotswap_comp 2>& 1 | tee no_fa3_fp8_nf4_hotswap_comp.txt
+    --out_dir=results/no_fa3_fp8_nf4_hotswap_comp 2>& 1 | tee results/no_fa3_fp8_nf4_hotswap_comp.txt
 
 python run_benchmark.py \
     --disable_fa3 --disable_fp8 --offload --quantize_t5 \
     --disable_hotswap \
     --disable_compile \
     --disable_recompile_error \
-    --out_dir=no_fa3_fp8_hotswap_comp 2>& 1 | tee no_fa3_fp8_hotswap_comp.txt
-
+    --out_dir=results/no_fa3_fp8_hotswap_comp 2>& 1 | tee results/no_fa3_fp8_hotswap_comp.txt
